29f85962063c0d9867583cd1e41c843080eeb5a4
Sofiene Djebali <t-sodje@microsoft.com>
2016-08-22 14:47:24 +0200

    APP_ID

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "f26df032-1c1a-4fdf-b457-ea1a55f11c3e",
+    appPassword: "6jHgDRj76QTzGhQVHZNYLBR"
 });
+
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
